a65e70bb466b5f4a964c93933180c1f9e427e88d
José Omar <joseomar_coppola@hotmail.com>
2025-01-06 21:43:08 +0100

    06-01-2025 more mathematics than programming SOS

---
+++ arrays&hashing/productOfArrayExceptSelf.js
@@ -0,0 +1,28 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements 
+of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = function (nums) {
+  let start = 1;
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    res.push(start);
+    start *= nums[i];
+    //[1,1,2,6]
+  }
+  let start2 = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    //starting from the last element of nums
+    res[i] *= start2;
+    start2 *= nums[i];
+  }
+  return res;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4])); //[24,12,8,6]
