90973aa68e634f0311f28431b09ea3738e390ef3
José Omar <joseomar_coppola@hotmail.com>
2025-01-07 20:33:52 +0100

    07-01-25 added strings bc the sudoku prob was impossible rn

---
+++ strings/reverseVowelsOfAString.js
@@ -0,0 +1,30 @@
+/*
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseVowels = function (s) {
+  const vowelsSet = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  let str = s.split('');
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (!vowelsSet.has(str[left])) {
+      left++;
+      continue;
+    }
+    if (!vowelsSet.has(str[right])) {
+      right--;
+      continue;
+    }
+    [str[left], str[right]] = [str[right], str[left]];
+    left++;
+    right--;
+  }
+  return str.join('');
+};
+
+console.log(reverseVowels('IceCreAm')); //AceCreIm
