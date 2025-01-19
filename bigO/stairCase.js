3c0088b60f6791795fdd54a454cbc7938092882f
José Omar <joseomar_coppola@hotmail.com>
2025-01-19 12:17:52 +0100

    19-11-2025, adding the stairCase solution to the bigO folder

--- bigO/stairCase.js
+++ bigO/stairCase.js
@@ -1,5 +1,21 @@
 // Given n the total number of steps in a staircase, and the ability to climb either 1 step or 2 steps
 //  at a time, how many distinct ways can you reach the top?
 
-const countStaircaseWays = function (n) {};
-console.log(countStaircaseWays(0));
+const countStaircaseWays = function (n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let waysToReachPreviousStep = 2; //(n-1)
+  let waysToReachTwoStepsBack = 1; //(n-2)
+  let totalWays = 0;
+
+  for (let i = 3; i <= n; i++) {
+    totalWays = waysToReachPreviousStep + waysToReachTwoStepsBack;
+
+    //update other variables (n-2 & n-1)
+    waysToReachTwoStepsBack = waysToReachPreviousStep;
+    waysToReachPreviousStep = totalWays;
+  }
+  return totalWays;
+};
+console.log(countStaircaseWays(3));
