b053b244743cdd12583d7a72acdb4fcdca553eec
omarscoppola <joseomar_coppola@hotmail.com>
2025-01-31 10:57:47 +0100

    31-01-2025, resolving the problem of kids with the greatest number of candies

--- arrays&hashing/kidsWithTheGreatestNumberOfCandies.js
+++ arrays&hashing/kidsWithTheGreatestNumberOfCandies.js
@@ -1,4 +1,4 @@
-/*
+/* 
     1431. Kids with the Greatest Number of Candies
     There are `n` kids with candies. You are given an integer array `candies`, where each `candies[i]` represents the number
     of candies the `ith` kid has, and an integer `extraCandies`, denoting the number of extra candies that you have.
@@ -17,8 +17,19 @@
         - Kid 3: they will have 5 + 3 = 8 candies, which is the greatest among the kids
         - Kid 4: they will have 1 + 3 = 4 candies, which is not the greatest among the kids
         - Kid 5: they will have 3 + 3 = 6 candies, which is the greatest among the kids
-*/
+ */
 
-const kidsWithTheGreatestNumberOfCandies = (candies, extraCandies) => {};
+const kidsWithTheGreatestNumberOfCandies = (candies, extraCandies) => {
+  //let's find first the kiddo with the greatest number of candies
+  let max = Math.max(...candies);
+  let result = [];
+  for (let i = 0; i < candies.length; i++) {
+    //this works because it's going to push true if the condition is met, and false otherwise
+    result.push(candies[i] + extraCandies >= max);
+  }
+  return result;
+};
+//time complexity: O(n)
+//space complexity: O(n)
 
 console.log(kidsWithTheGreatestNumberOfCandies([2, 3, 5, 1, 3], 3)); //[true, true, true, false, true]
