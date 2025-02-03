56f0dd2b6b5452c5f21fc99570c6f743085ab084
José Omar <joseomar_coppola@hotmail.com>
2025-02-03 07:45:53 +0100

    03-02-2025, can place flowers arrays problem

---
+++ arrays&hashing/canPlaceFlowers.js
@@ -0,0 +1,34 @@
+/* You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers 
+cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means 
+not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without 
+violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true 
+
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+*/
+
+const canPlaceFlowers = (flowerBed, n) => {
+  //iterating over the array
+  for (let i = 0; i < flowerBed.length; i++) {
+    if (
+      flowerBed[i] === 0 &&
+      (i === 0 || flowerBed[i - 1] !== 1) &&
+      (i === flowerBed.length - 1 || flowerBed[i + 1] !== 1)
+    ) {
+      flowerBed[i] += 1;
+      n--;
+    }
+  }
+  return n === 0 ? true : false;
+};
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
