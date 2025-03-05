5d23ca93410716009fa806e3460f29edcfd74c0e
omarscoppola <joseomar_coppola@hotmail.com>
2025-03-05 12:30:21 +0100

    03-05-2025, first binary tree problem ?

---
+++ binaryTreeDFS/maximumDepthOfBinaryTree.js
@@ -0,0 +1,24 @@
+/* 104. Maximum Depth Of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the 
+root node down to the farthest leaf node.
+
+Example 1:
+                (3️⃣)
+               /    \
+            (9️⃣)  (2️⃣0️⃣)
+                  /      \
+              (1️⃣5️⃣)    (7️⃣)
+              
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2 */
+
+const maxDepth = (root) => {};
+
+console.log([3, 9, 20, null, null, 15, 7]);
