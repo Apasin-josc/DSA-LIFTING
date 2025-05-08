72b8ec4f05faa6ebf670b5e9989983c5c9af78eb
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-08 11:21:28 +0200

    definitely idk how to add a input to the cli

--- python/Hello_World.py
+++ python/Hello_World.py
@@ -1,1 +1,6 @@
-print('Hello World!');
+#birth_year = int(input('what year were you born ?'))
+birth_year = 1997
+current_year = 2025
+age = current_year - birth_year
+message = f'you are {age} years old'
+print(message)
