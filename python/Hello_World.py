4c69a7807000b08cadbc4574d02707c8dd92762d
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-13 08:19:23 +0200

    adding a unique occurrences problem to the repo

--- python/Hello_World.py
+++ python/Hello_World.py
@@ -1,21 +1,4 @@
-def highest_even(li):
-    highest_even_number = 0
-    for num in li:
-        if num % 2 == 0 and num >= highest_even_number:
-            highest_even_number = num
-        elif not num % 2 == 0 in li:
-            return ('there is no even numbers on this list')
-            
-        return highest_even_number
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 
-print(highest_even([10,2,3,4,8,11]))
-""" print(highest_even([1,3,5])) """
-
-
-def highest_even_tuto(li):
-    evens = []
-    for item in li:
-        if item % 2 == 0:
-            evens.append(item)
-    return max(evens)
-print(highest_even_tuto([10,2,3,4,8,12]))
+duplicates = list(set([char for char in some_list if some_list.count(char) > 1]))
+print(duplicates)
