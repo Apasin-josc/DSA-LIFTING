18d2588221e57b65863e8de61f6f37d16be9e5ec
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-20 08:02:01 +0200

    solving the mergeStringsAlternately problem, STRINGS ARE INMUTABLE

--- python/roadmap/mergeStringsAlternately.py
+++ python/roadmap/mergeStringsAlternately.py
@@ -29,7 +29,16 @@ word2:    p   q
 merged: a p b q c   d """
 
 def mergeStringsAlternately(word1: str, word2: str) -> str:
-    print('hello there')
-
+    merged = []
+    i = 0
+    j = 0
+    while i < len(word1) or j < len(word2):
+        if i < len(word1):
+            merged.append(word1[i])
+            i += 1
+        if j < len(word2):
+            merged.append(word2[j])
+            j += 1
+    return ''.join(merged)
 
 print(mergeStringsAlternately("abc", "pqr"))
