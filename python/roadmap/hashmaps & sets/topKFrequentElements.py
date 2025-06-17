e868ee2f5de2110edde0889c91fd2b7aa69533e1
omarscoppola <joseomar_coppola@hotmail.com>
2025-06-17 12:55:26 +0200

    flagging the topkfrequentelements

---
+++ python/roadmap/hashmaps & sets/topKFrequentElements.py
@@ -0,0 +1,18 @@
+""" 
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+ """
+
+from typing import List
+def topKFrequent(nums:List[int], k:int) -> List[int]:
+    pass
+    
+
+print(topKFrequent([1,1,1,2,2,3], 2))
