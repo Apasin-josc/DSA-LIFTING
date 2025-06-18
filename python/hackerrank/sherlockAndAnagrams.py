a92d4616a0a5601eb437149f04593519f1c67ea5
omarscoppola <joseomar_coppola@hotmail.com>
2025-06-18 08:53:38 +0200

    finishing the twostrings problem and addnig a MOAB problem to the repo, ._.

---
+++ python/hackerrank/sherlockAndAnagrams.py
@@ -0,0 +1,27 @@
+"""
+Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string.  
+Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+
+Example
+s = mom
+
+The list of all anagrammatic pairs is [m, m], [mo, om] at positions [[0], [2]], [[0, 1], [1, 2]] respectively.
+
+Function Description
+Complete the function sherlockAndAnagrams in the editor below.
+
+sherlockAndAnagrams has the following parameter(s):
+• string s: a string
+
+Returns
+• int: the number of unordered anagrammatic pairs of substrings in s
+
+Input Format
+The first line contains an integer q, the number of queries.  
+Each of the next q lines contains a string s to analyze.
+"""
+
+def sherlockAndAnagrams(s: str) -> int:
+    pass
+
+sherlockAndAnagrams("mom")
