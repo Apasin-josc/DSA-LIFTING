5e8c3524919c587dafa1ebec6dbd97aca4bb5b20
José Omar <joseomar_coppola@hotmail.com>
2025-06-20 16:43:56 +0200

    adding to the repo a valid anagram problem

---
+++ python/neetcode/arrays & hashing/validAnagram.py
@@ -0,0 +1,32 @@
+""" 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false 
+"""
+def isAnagram(s:str, t:str) -> bool:
+    hashMap_s = {}
+    for char in s:
+        if char in hashMap_s:
+            hashMap_s[char] += 1
+        else:
+            hashMap_s[char] = 1
+
+    hashMap_t = {}
+    for char in t:
+        hashMap_t[char] = hashMap_t.get(char, 0) + 1
+    
+    if hashMap_s.items() != hashMap_t.items():
+        return False
+    else:
+        return True
+
+
+
+print(isAnagram("anagram", "nagaram"))
