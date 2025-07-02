83ae5c033aaab3d9a54773e2578389418bf31d0f
omarscoppola <joseomar_coppola@hotmail.com>
2025-07-02 08:12:19 +0200

    giving it another proach from behind

--- python/educative.io/lists/removeEvenIntegers.py
+++ python/educative.io/lists/removeEvenIntegers.py
@@ -1,13 +1,12 @@
 from typing import List
 def remove_even(lst:List[int]) -> List[int]:
-    ans = []
-    for i in range(len(lst)):
-        if lst[i] % 2 == 0:
-            pass
-        else:
-            ans.append(lst[i])
     
-    return ans
+    for i in range(len(lst) - 1, -1, -1):
+        if lst[i] % 2 == 0:
+            #del lst[i]
+            lst.remove(lst[i])
+    
+    return lst
 
 
 print(remove_even([1,3,-2,4,7]))
