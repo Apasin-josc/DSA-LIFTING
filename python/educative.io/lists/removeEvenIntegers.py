06d6869cdf2984d235e7811fa343afe18b230dcd
omarscoppola <joseomar_coppola@hotmail.com>
2025-07-02 08:17:25 +0200

    adding a list comprehension example also from the same problem

--- python/educative.io/lists/removeEvenIntegers.py
+++ python/educative.io/lists/removeEvenIntegers.py
@@ -3,10 +3,14 @@ def remove_even(lst:List[int]) -> List[int]:
     
     for i in range(len(lst) - 1, -1, -1):
         if lst[i] % 2 == 0:
-            #del lst[i]
-            lst.remove(lst[i])
+            del lst[i]
     
     return lst
 
 
 print(remove_even([1,3,-2,4,7]))
+
+def remove_even_list_comprehension(lst:List[int]) -> List[int]:
+    return [number for number in lst if number % 2 != 0]
+
+print(remove_even_list_comprehension([3,4,40,31,32]))
