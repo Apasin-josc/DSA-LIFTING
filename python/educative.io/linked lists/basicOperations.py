e791d0679ea210b4f6e1d478406d11311b827189
omarscoppola <joseomar_coppola@hotmail.com>
2025-07-08 11:59:48 +0200

    adding to the project a prepend and pop_firsto perations for the linkedlists

--- python/educative.io/linked lists/basicOperations.py
+++ python/educative.io/linked lists/basicOperations.py
@@ -31,9 +31,61 @@ class LinkedList:
     def print_list(self):
         temp = self.head
         while temp is not None:
-            print(temp.value)
+            print(temp.value, '->')
             temp = temp.next
-
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp 
 
 list = LinkedList(20)
+list.append(50)
+list.prepend(1)
+list.pop_first()
 list.print_list()
