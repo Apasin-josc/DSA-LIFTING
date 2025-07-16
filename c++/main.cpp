e8244f2d2eb0bf10de6ad1f6cd2e48a02dba6203
omarscoppola <joseomar_coppola@hotmail.com>
2025-07-16 10:30:58 +0200

    learning a little more of c++

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, C++ desde VS Code!" << endl;
+/* int main()
+{
+    int a, b, c;
+
+    cout << "enter two numbers";
+
+    cin >> a >> b;
+    
+    c = a + b;
+
+    cout << "the addition is" << " " << c;
+
+    return 0;
+} */
+
+
+int main()
+{
+    string name;
+
+    cout << "enter your name";
+
+    getline(cin, name);
+
+    cout << "hello there " << name;
+
     return 0;
 }
