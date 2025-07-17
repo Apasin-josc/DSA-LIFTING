c473e2b0c68a801990f37ae4608e50d25dea7fb2
omarscoppola <joseomar_coppola@hotmail.com>
2025-07-17 09:06:36 +0200

    adding a program to enter your name on the screen lol

--- java/dsa/src/App.java
+++ java/dsa/src/App.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("enter your name ");
+        name = sc.nextLine();
+        System.out.println("hello there " + name);
     }
 }
