1cbb979f21ad81999732571e82b646e4a1cab1e7
José Omar <joseomar_coppola@hotmail.com>
2025-08-06 18:48:14 +0200

    back at it again with codegym bros

---
+++ java/codegym/App/src/FixingAge/FixingAge.java
@@ -0,0 +1,20 @@
+package FixingAge;
+
+public class FixingAge {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println("Age: " + person.age);
+        person.adjustAge(person.age);
+        /* System.out.println(person.age); */
+        System.out.println("Adjusted age: " + person.age);
+    }
+
+    public static class Person {
+        public int age = 20;
+
+        public void adjustAge(int age) {
+            this.age = age + 20;
+            System.out.println("The age in adjustAge() is: " + age);
+        }
+    }
+}
---
+++ java/codegym/README.md
@@ -0,0 +1,4 @@
+over the App folder ex `PS C:\Users\joseo\Desktop\DSA-LIFTING\java\codegym\App> java -cp bin FixingAge.FixingAge`
+
+javac -d bin src/FixingAge/FixingAge.java
+java -cp bin FixingAge.FixingAge
